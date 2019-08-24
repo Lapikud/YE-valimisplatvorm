e5a249cbeb859e5d1d7979d5463c098ee50358d4
Terje Russka <terjerusska@gmail.com>
2019-08-24 20:17:10 +0300

    add applicant creation, edit, and request from the database

--- backend/src/main/java/backend/controller/Controller.java
+++ backend/src/main/java/backend/controller/Controller.java
@@ -8,20 +8,55 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.io.ByteArrayInputStream;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class Controller {
 
     @Autowired
     ApplicantRepository repository;
+
+    @GetMapping(value = "/applicant/{id}")
+    public ResponseEntity<Applicant> getApplicant(@PathVariable Long id) {
+        Optional<Applicant> applicant = repository.findById(id);
+        return applicant.isPresent() ? ResponseEntity.ok(applicant.get()) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping(value = "/applicant/{matrikkel}")
+    public ResponseEntity<List<Applicant>> getAllApplicantsByMatrikkel(@PathVariable Long matrikkel) {
+        List<Applicant> applicants = repository.findByMatrikkel(matrikkel);
+        return applicants.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(applicants);
+    }
+
+    @GetMapping(value = "/applicant/all")
+    public ResponseEntity<List<Applicant>> getAllApplicants() {
+        List<Applicant> applicants = repository.findAll();
+        return applicants.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(applicants);
+    }
+
+    @PostMapping(value = "/applicant/{id}")
+    public ResponseEntity<Applicant> editApplicant(@PathVariable Long id, @Valid @RequestBody Applicant editApplicant) {
+        Optional<Applicant> applicantDb = repository.findById(id);
+        if (applicantDb.isPresent()) {
+            editApplicant.setId(applicantDb.get().getId());
+            editApplicant.setCreatedDate(applicantDb.get().getCreatedDate());
+            repository.save(editApplicant);
+            return ResponseEntity.ok(editApplicant);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping(value = "/applicant")
+    public void addApplicant(@Valid @RequestBody Applicant newApplicant) {
+        repository.save(newApplicant);
+    }
 
     @PostMapping(value = "/generate/application")
     public ResponseEntity<InputStreamResource> generatePDF(@Valid @RequestBody Applicant newApplicant) {
--- backend/src/main/java/backend/repository/ApplicantRepository.java
+++ backend/src/main/java/backend/repository/ApplicantRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
-    List<Applicant> findByLastName(String lastName);
+    List<Applicant> findByMatrikkel(Long matrikkel);
     List<Applicant> findAll();
 }
